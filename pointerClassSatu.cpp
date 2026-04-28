80dc5577e9565c8263b876d93051430f7d05db00
rafiammar014 <rafiammar014@gmail.com>
2026-04-28 15:28:13 +0700

    Menambahkan implementasi method pada class siswa dan orang serta perbaikan penamaan
    - Mengimplementasikan method displayAll untuk menampilkan id dan nama
    - Mengimplementasikan method setNama pada class orang
    - Mengimplementasikan method setId pada class siswa
    - Memperbaiki penamaan variabel dari Id menjadi id
    - Menyamakan nama method showplayAll menjadi displayAll

--- pointerClassSatu.cpp
+++ pointerClassSatu.cpp
@@ -22,3 +22,16 @@ public:
     void setId(int pId);
     void showplayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{             
+    cout << id << endl << a.nama << endl;
+}
+void orang ::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}  

d5a301bbcdd46775755ab3c9e20c6776edb86f26
rafiammar014 <rafiammar014@gmail.com>
2026-04-28 15:45:15 +0700

    Menambahkan relasi friend antara class manusia dan pelajar

---
+++ friendFunctionDua.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class pelajar;
+class manusia
+{
+public:
+    void showNimPelajar(pelajar &x);
+};
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNimPelajar(pelajar &x);
+};
